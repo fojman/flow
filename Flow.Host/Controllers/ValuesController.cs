bd02591cddcb1fef4874583e877c903540541d26
Evgen Yukhimovich <evgen.yukhimovich@pm.bet>
2019-12-25 17:13:23 +0200

    BL-77 Added Health controller + minor fixes

--- Flow.Host/Controllers/ValuesController.cs
+++ Flow.Host/Controllers/ValuesController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.Text;
 using Microsoft.Extensions.Logging;
 
 namespace Flow.Controllers
